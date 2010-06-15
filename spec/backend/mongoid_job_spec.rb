be2f8cfbc50b4bac97fc950c720f78e259b211c1
Don Buchanan <mail@donaldbuchanan.com>
2010-06-16 08:39:16 +1000

    added before and after specs

--- spec/backend/mongoid_job_spec.rb
+++ spec/backend/mongoid_job_spec.rb
@@ -13,4 +13,29 @@ describe Delayed::Backend::Mongoid::Job do
   end
   
   it_should_behave_like 'a backend'
+  
+  
+  describe "before_fork" do
+    after do
+      ::Mongoid.master.connection.close
+    end
+    
+    it "should disconnect" do
+      lambda do
+        Delayed::Backend::Mongoid::Job.before_fork
+      end.should change { !!Mongoid.master.connection.connected? }.from(true).to(false)
+    end
+  end
+
+  describe "after_fork" do
+    before do
+      ::Mongoid.master.connection.close
+    end
+    
+    it "should call reconnect" do
+      lambda do
+        Delayed::Backend::Mongoid::Job.after_fork
+      end.should change { !!Mongoid.master.connection.connected? }.from(false).to(true)
+    end
+  end
 end
